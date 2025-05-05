667e93bbaaff1edf91f1f5e90d6deb017c4c9d57
Brian Chen <debriann07@gmail.com>
2025-05-05 11:23:17 -0700

    backend URI update

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   // Replace <YOUR_BACKEND_URL> with the actual URL of your Flask backend, e.g., "http://47.229.136.34:5000"
-  const backendUrl = "https://www.infinia.chat";
+  const backendUrl = "https://api.shazoom.app";
 
   return (
     <div className="App">
